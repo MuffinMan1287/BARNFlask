1ee2c1a2566f245ba25648ef3c455513b27aa901
MuffinMan1287 <arnavk1321@gmail.com>
2023-02-03 14:24:41 -0800

    hopefully wrks

---
+++ api/qb.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource # used for REST API building
+from datetime import datetime
+
+from model.qbs import Qb
+
+user_api = Blueprint('user_api', __name__,
+                   url_prefix='/api/users')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+api = Api(user_api)
+
+class UserAPI:        
+    class _Create(Resource):
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            ''' Avoid garbage in, error checking '''
+            # validate name
+            name = body.get('name')
+            if name is None or len(name) < 2:
+                return {'message': f'Name is missing, or is less than 2 characters'}, 210
+            # validate uid
+            
+            atts = body.get('atts')
+            if atts is None or len(atts) < 2:
+                return {'message': f'Atts is missing, or is less than 2 characters'}, 210
+            
+            comps = body.get('comps')
+            if comps is None or len(comps) < 2:
+                return {'message': f'Comps is missing, or is less than 2 characters'}, 210
+
+            yards = body.get('yards')
+            if yards is None or len(yards) < 2:
+                return {'message': f'Yards is missing, or is less than 2 characters'}, 210
+
+            tds = body.get('tds')
+            if tds is None:
+                return {'message': f'TDs is missing, or is less than 2 characters'}, 210
+            
+            ''' #1: Key code block, setup USER OBJECT '''
+            uo = Qb(name=name, 
+                    atts=atts,
+                    comps=comps,
+                    yards=yards,
+                    tds=tds)
+            
+            ''' #2: Key Code block to add user to database '''
+            # create user in database
+            qb = uo.create()
+            # success returns json of user
+            if qb:
+                return jsonify(qb.read())
+            # failure returns error
+            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 210
+
+    class _Read(Resource):
+        def get(self):
+            qbs = Qb.query.all()    # read/extract all users from database
+            json_ready = [qb.read() for qb in qbs]  # prepare output in json
+            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+
+    # building RESTapi endpoint
+    api.add_resource(_Create, '/create')
+    api.add_resource(_Read, '/')
---
+++ model/qbs.py
@@ -0,0 +1,179 @@
+""" database dependencies to support sqliteDB examples """
+from random import randrange
+from datetime import date
+import os, base64
+import json
+
+from __init__ import app, db
+from sqlalchemy.exc import IntegrityError
+
+
+''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
+
+# Define the Post class to manage actions in 'posts' table,  with a relationship to 'users' table
+class Make(db.Model):
+    __tablename__ = 'posts'
+
+    # Define the Notes schema
+    id = db.Column(db.Integer, db.ForeignKey('pys.id'), primary_key=True)
+    note = db.Column(db.Text, unique=False, nullable=False)
+    image = db.Column(db.String, unique=False)
+    # Define a relationship in Notes Schema to userID who originates the note, many-to-one (many notes to one user)
+
+    # Constructor of a Notes object, initializes of instance variables within object
+
+
+    # Returns a string representation of the Notes object, similar to java toString()
+    # returns string
+
+    # CRUD create, adds a new record to the Notes table
+    # returns the object added or None in case of an error
+
+
+# Define the User class to manage actions in the 'users' table
+# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# -- a.) db.Model is like an inner layer of the onion in ORM
+# -- b.) User represents data we want to store, something that is built on db.Model
+# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
+class Qb(db.Model):
+    __tablename__ = 'freeagents'  # table name is plural, class name is singular
+
+    # Define the User schema with "vars" from object
+    id = db.Column(db.Integer, primary_key=True)
+    _name = db.Column(db.String(255), unique=False, nullable=False)
+    _atts = db.Column(db.String(255), unique=False, nullable=False)
+    _comps = db.Column(db.String(255), unique=False, nullable=False)
+    _yards = db.Column(db.String(255), unique=False, nullable=False)
+    _tds = db.Column(db.String(255), unique=False, nullable=False)
+
+    # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
+
+    # constructor of a User object, initializes the instance variables within object (self)
+    def __init__(self, name, atts, comps, yards, tds):
+        self._name = name    # variables with self prefix become part of the object, 
+        self._atts = atts
+        self._comps = comps
+        self._yards = yards
+        self._tds = tds
+
+    # a name getter method, extracts name from object
+    @property
+    def name(self):
+        return self._name
+    
+    # a setter function, allows name to be updated after initial object creation
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    @property
+    def atts(self):
+        return self._atts
+    
+    # a setter function, allows name to be updated after initial object creation
+    @name.setter
+    def atts(self, atts):
+        self._atts = atts
+
+    @property
+    def comps(self):
+        return self._comps
+    
+    # a setter function, allows name to be updated after initial object creation
+    @name.setter
+    def comps(self, comps):
+        self._comps = comps
+
+    @property
+    def yards(self):
+        return self._yards
+    
+    # a setter function, allows name to be updated after initial object creation
+    @name.setter
+    def yards(self, yards):
+        self._yards = yards
+
+    @property
+    def tds(self):
+        return self._tds
+    
+    # a setter function, allows name to be updated after initial object creation
+    @tds.setter
+    def tds(self, tds):
+        self._tds = tds
+    
+    # output content using str(object) in human readable form, uses getter
+    # output content using json dumps, this is ready for API response
+    def __str__(self):
+        return json.dumps(self.read())
+
+    # CRUD create/add a new record to the table
+    # returns self or None on error
+    def create(self):
+        try:
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    # CRUD read converts self to dictionary
+    # returns dictionary
+    def read(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "atts" : self.atts,
+            "comps" : self.comps,
+            "yards" : self.yards,
+            "tds": self.tds
+        }
+
+    # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self, name, atts, comps, yards, tds):
+        """only updates values with length"""
+        if len(name) > 0:
+            self.name = name
+        if len(atts) > 0:
+            self.atts = atts
+        if len(comps) > 0:
+            self.comps = comps
+        if len(yards) > 0:
+            self.yards = yards
+        if len(tds) > 0:
+            self.tds = tds
+        db.session.commit()
+        return self
+
+    # CRUD delete: remove self
+    # None
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+
+"""Database Creation and Testing """
+
+
+# Builds working data for testing
+def initQbs():
+    """Create database and tables"""
+    db.create_all()
+    """Tester data for table"""
+    p1 = Qb(name='Patrick Mahomes', atts='648', comps='435', yards='5250', tds='41')
+    p2 = Qb(name='Justin Herbert', atts='699', comps='477', yards='4739', tds='25')
+    p3 = Qb(name='Tom Brady', atts='733', comps='490', yards='4694', tds='25')
+    qbs = [p1, p2, p3]
+    for qb in qbs:
+        try:
+            qb.create()
+        except IntegrityError:
+            '''fails with bad or duplicate data'''
+            db.session.remove()
+            print(f"Records exist, duplicate email, or error: {user.name}")
+
+            
