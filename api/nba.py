b6c7a0410e59d5a81006d3e8485e0f4d01acd8a3
benjaminlee24 <bencabrallee@gmail.com>
2023-02-10 13:16:22 -0800

    idk

--- api/nba.py
+++ api/nba.py
@@ -1,10 +1,15 @@
-from nba_api.live.nba.endpoints import scoreboard
+import http.client
 
-# Today's Score Board
-games = scoreboard.ScoreBoard()
+conn = http.client.HTTPSConnection("sportspage-feeds.p.rapidapi.com")
 
-# json
-games.get_json()
+headers = {
+    'X-RapidAPI-Key': "SIGN-UP-FOR-KEY",
+    'X-RapidAPI-Host': "sportspage-feeds.p.rapidapi.com"
+    }
 
-# dictionary
-games.get_dict()
+conn.request("GET", "/teams?league=%3CREQUIRED%3E", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
