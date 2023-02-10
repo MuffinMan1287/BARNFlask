7fc7f12fc1c504d9090cdc762fa35890aba83b06
benjaminlee24 <bencabrallee@gmail.com>
2023-02-10 13:38:09 -0800

    api

--- api/nba.py
+++ api/nba.py
@@ -1,13 +1,13 @@
 import http.client
 
-conn = http.client.HTTPSConnection("sportspage-feeds.p.rapidapi.com")
+conn = http.client.HTTPSConnection("nba-player-props-odds.p.rapidapi.com")
 
 headers = {
-    'X-RapidAPI-Key': "SIGN-UP-FOR-KEY",
-    'X-RapidAPI-Host': "sportspage-feeds.p.rapidapi.com"
+    'X-RapidAPI-Key': "0608f45064msh40e1e6383c4be53p176cf6jsn49eacc437480",
+    'X-RapidAPI-Host': "nba-player-props-odds.p.rapidapi.com"
     }
 
-conn.request("GET", "/teams?league=%3CREQUIRED%3E", headers=headers)
+conn.request("GET", "/get-player-odds-for-event?eventId=22200&bookieId=1%3A4%3A5%3A6%3A7%3A8%3A9%3A10&marketId=1%3A2%3A3%3A4%3A5%3A6&decimal=true&best=true", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
