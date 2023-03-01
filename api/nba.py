609940e5cf53cf1b11a0843c4f28f63d48270f1c
benjaminlee24 <bencabrallee@gmail.com>
2023-03-01 12:12:06 -0800

    api

--- api/nba.py
+++ api/nba.py
@@ -1,14 +1,46 @@
-import requests
+from flask import Blueprint, request
+from flask_restful import Api, Resource
+from .. import db
+from ..model.comment import Comment
 
-url = "https://nba-player-props-odds.p.rapidapi.com/get-player-odds-for-event"
+comment_blueprint = Blueprint("comment", __name__)
+comment_api = Api(comment_blueprint)
 
-querystring = {"eventId":"22200","bookieId":"1:4:5:6:7:8:9:10","marketId":"1:2:3:4:5:6","decimal":"true","best":"true"}
+class CommentsAPI(Resource):
+  def get(self):
+    id = request.args.get("id")
+    comment = db.session.query(Comment).get(id)
+    if comment:
+      return comment.to_dict()
+    return {"message": "player not found"}, 404
 
-headers = {
-	"X-RapidAPI-Key": "0608f45064msh40e1e6383c4be53p176cf6jsn49eacc437480",
-	"X-RapidAPI-Host": "nba-player-props-odds.p.rapidapi.com"
-}
+  def post(self):
+    name = request.args.get("name")
+    body = request.args.get("body")
+    
+    new_comment = Comment(name=name, body=body)
+    print(new_comment.to_dict())
+    if new_comment:
+      db.session.add(new_comment)
+      db.session.commit()
+      return
+    return {"message": "error creating comment"}, 404
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
+  def delete(self):
+    id = request.args.get("id")
+    comment = db.session.query(Comment).get(id)
+    try:
+      db.session.add(comment)
+      db.session.commit()
+    except Exception as e:
+      db.session.rollback()
+      return "error while creating comment: " + str(e)
+    return {"message": "error creating comment"}, 404
+  
+class ListComments(Resource):
+  def get(self):
+    comment = db.session.query(Comment).all()
+    return [comment.to_dict() for comment in comment]
+  
+comment_api.add_resource(ListComments, "/list-comments")
+comment_api.add_resource(CommentsAPI, "/comments")
